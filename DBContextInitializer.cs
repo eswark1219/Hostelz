eae72d47fe1031b0562fe69a4536db5ad2834351
Eswar <35649315+keswar@users.noreply.github.com>
2020-10-01 20:16:00 +0530

    Add files via upload
    application db  service

---
+++ DBContextInitializer.cs
@@ -0,0 +1,34 @@
+﻿using Hostelz_FunctionalService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hostelz_DBService
+{
+    public static class DBContextInitializer
+    {
+
+        public static async Task Initialize(DataProtectionKeysContext dataProtectionKeysContext, ApplicationDbContext applicationDBContext,
+            IFunctionalService functionalService)
+        {
+            // Check, if db DataProtectionKeysContext is created
+            // Check, if db ApplicationDbContext is created
+            await dataProtectionKeysContext.Database.EnsureCreatedAsync();
+            await applicationDBContext.Database.EnsureCreatedAsync();
+
+
+            // Check, if db contains any users. If db is not empty, then db has been already seeded
+            if (applicationDBContext.ApplicationUsers.Any())
+            {
+                return;
+            }
+
+            // If empty create Admin User and App User
+            await functionalService.CreateDefaultAdminUser();
+            await functionalService.CreateDefaultUser();
+        }
+
+    }
+}
